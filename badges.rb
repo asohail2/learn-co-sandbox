d9c4586730ed6f9f9727110cdfceb57b8c42031b
Aqsa Sohail <aqsasohail786@gmail.com>
2019-02-17 21:06:58 +0000

    Automatically backed up by Learn

--- badges.rb
+++ badges.rb
@@ -11,12 +11,20 @@ def batch_badge_creator(speakers)
   badge_messages
 end
 
+def assign_rooms(speakers)
+  speakers.each_with_index do |speakers, index|
+    puts "Hello, " + speakers + "! "
+    puts "You'll be assigned to room " + index.to_s + "!"
+  end
+end
+
 def print_badges(speakers)
   puts batch_badge_creator(speakers)
+  assign_rooms(speakers)
 end
 
 name = ["Edsgar", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz" ]
 print_badges(name)
 
-def assign_rooms
+
 
